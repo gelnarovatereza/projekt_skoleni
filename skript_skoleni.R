8a09785dbef7d16e188c53fce5cfc47e6efd1e11
gelnarovatereza <tereza.gelnarova@aopk.gov.cz>
2025-05-14 15:32:45 +0200

    troubleshoot

--- skript_skoleni.R
+++ skript_skoleni.R
@@ -6,3 +6,13 @@
 # pokus zaznamenani zmeny
 
 # zmena 3
+# Read about PAT
+usethis::gh_token_help()
+
+# List all git settings
+usethis::git_sitrep()
+
+# Check if PAT is set
+gh::gh_whoami()
+
+usethis::use_github()
